d153c8d2578641dbdab18db5360b6c433d941bd6
Yurii Vavrin <yuravavrin@gmail.com>
2020-03-16 15:58:29 +0100

    added socket logic in service

--- src/app/live-bets.service.ts
+++ src/app/live-bets.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, BehaviorSubject } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { LiveBet } from './interfaces/live-bet.interface';
+import { Socket } from 'ngx-socket-io';
 import { environment } from '../environments/environment';
 
 @Injectable({
@@ -16,11 +17,48 @@ export class LiveBetsService {
 
   liveBets: Observable<LiveBet[]> = this.liveBetsData.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private socket: Socket) {
+    this.startSocketPulling();
+    socket.on('bet-updated', this.onBetUpdated.bind(this));
+  }
+
+  onBetUpdated(newBets: LiveBet[]) {
+    // console.log(performance.now());
+    let liveBets = this.liveBetsData.getValue();
+
+    if (!!liveBets.length) {
+      newBets.forEach(newBet => {
+        if (liveBets.some(bet => bet.id === newBet.id)) {
+          const index = liveBets.findIndex(bet => bet.id === newBet.id);
+          liveBets.splice(index, 1, newBet);
+        } else {
+          liveBets.push(newBet);
+        }
+      });
+    } else {
+      liveBets = newBets;
+    }
+
+    this.liveBetsData.next(liveBets);
+    console.log(newBets);
+    // console.log(performance.now());
+  }
+
+  startSocketPulling(rate = 2) {
+    this.http.get<void>(this.baseUrl + 'pulling/start?rate=' + rate)
+      .pipe(
+        catchError(this.handleError<void>('startSocket'))
+      ).subscribe();
+  }
+
+  stopSocketPulling() {
+    this.http.get<void>(this.baseUrl + 'pulling/stop')
+      .pipe(
+        catchError(this.handleError<void>('startSocket'))
+      ).subscribe();
+  }
 
   getBets(): void {
-    console.log('hello');
-
     this.http.get<LiveBet[]>(this.baseUrl + 'bets')
       .pipe(
         catchError(this.handleError<LiveBet[]>('getHeroes', []))
